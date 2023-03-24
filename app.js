640c48820f1f47755af6ee66f3d042ec4bcbe520
Shruti2906 <65824891+Shruti2906@users.noreply.github.com>
2023-03-24 20:10:04 +0530

    employee CRUD

--- app.js
+++ app.js
@@ -3,32 +3,50 @@ const express = require('express')//its like import statement
 const app = express() //now app can be used function
 const port = 3000
 
+
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:4200',
+  allowedHeaders: ['Content-Type'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
+
 const employees = []
 
-app.get('/api/getData', (req, res) => {
+app.use(express.json());
 
-    res.status(200).json(employees)
+// app.use((req, res, next)=>{
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   next();
+// });
+
+
+app.get('/get', (req, res) => {
+
+  res.status(200).json(employees)
 //   res.status(200).json({
 //     id: 1,
 //     name: "employee1"
 //   })
 })
 
-app.use(express.json());
 
-app.post('/api/add', (req, res) => {
+app.post('/add', (req, res) => {
    
     const index = employees.findIndex(e => e.id === req.body.id);
     if(index === -1){
       employees.push(req.body);
-      res.status(200).json({msg: "added success"});
+      return res.status(200).json({msg: "added success"});
     }
     else{
-      res.status(400).json({msg: "Employee Exist with Same id..!"});
+     return res.status(400).json({msg: "Employee Exist with Same id..!"});
     }
 })
 
-app.put('/api/put', (req, res)=>{
+app.put('/put', (req, res)=>{
 
     const index = employees.findIndex(e => e.id === req.body.id);
     if(index !== -1){
@@ -43,23 +61,36 @@ app.put('/api/put', (req, res)=>{
 
 })
 
-app.delete('/api/delete', (req, res) => {
+app.delete('/delete/:id', (req, res) => {
   
-  const id = req.body.id;
+  // const id = req.body.id;
+  const id = req.params.id;
+  console.log('delete id : ')
   console.log(id)
-  const index = employees.findIndex(e => e.id === id);
-  if (index !== -1) {
-    employees.splice(index, 1);
+  // const index = employees.findIndex(e => e.id === id);
+  const index = employees.findIndex(e => e.id === parseInt(id));
+  console.log(index)
+  if (index === -1) {
+    console.log('not found')
+  res.status(400).json({msg: "delete failed"});
+   
   }
-  res.status(200).json({msg: "deleted success"});
+  else{
+    console.log('found')
+    employees.splice(index, 1);
+    res.status(200).json({msg: "deleted success"});
+  }
 })
 
 app.listen(port, () => {
   console.log(`Example app listenin on port ${port}`)
 })
+//OR simply app.listen(4000)
+
+//app.listen(4000);
 
 // http://localhost:3000/
 // body json
 // {
 //     "msg": "addes success"
-//   }
+//  }
